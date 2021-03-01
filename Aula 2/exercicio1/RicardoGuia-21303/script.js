9c6a27c84494598d3c2be7ea9dfc3604c458dae6
Ricardo <ricardo.guia@atlanse.pt>
2021-03-01 17:47:14 +0000

    correction

--- Aula 2/exercicio1/RicardoGuia-21303/script.js
+++ Aula 2/exercicio1/RicardoGuia-21303/script.js
@@ -1,9 +1,9 @@
 // Your JS Script here
 
 function readInputFromText() {
-	const textFromInput = document.getElementById("inputText").value
+	const textFromInput = document.getElementById("inputText").value;
 	if (textFromInput) {
-		document.getElementById("viewerText").innerText = textFromInput
+		document.getElementById("viewerText").innerText = textFromInput;
 	} else {
 		alert("Escreva algo no input");
 	}
