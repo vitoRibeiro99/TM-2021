0aecc31916d92d6411a4c9d04427f498115e9470
Ricardo <ricardo.guia@atlanse.pt>
2021-03-08 15:43:51 +0000

    Aula 3 Exercicio 2 Turma A

---
+++ Aula 3/exercicio4/ricardoGuia-21301/script.js
@@ -0,0 +1,20 @@
+// Your JS Script here
+
+let imgCounter = 0;
+
+function addImage ()
+{
+	imgCounter++;
+	let img = document.createElement("img")
+	img.src="https://placeimg.com/250/150/" + imgCounter
+	
+	const images = document.getElementById("imageContainer")
+	images.appendChild(img)
+
+}
+
+
+function delImage ()
+{
+
+}
